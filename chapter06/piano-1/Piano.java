e7c1c3197ca23f89323386d9e451d30f69648b91
Russell Gordon <rgordon@lcs.on.ca>
2019-04-21 10:26:06 -0400

    Track how many frames have been animated

--- chapter06/piano-1/Piano.java
+++ chapter06/piano-1/Piano.java
@@ -15,12 +15,18 @@ public class Piano extends World
     private String[] peopleInClass = {"Chelsea", "Sarah", "Jack", "Maxwell", 
     "Justy", "Peter", "Jeewoo", "Brandon", "Minah", "Gavin"};
     
+    // Track how many frames have been animated
+    private int frames;
+    
     /**
      * Make the piano.
      */
     public Piano() 
     {
         super(800, 340, 1);
+        
+        // No frames animated at start
+        frames = 0;
     }
     
     /**
@@ -30,5 +36,11 @@ public class Piano extends World
     {
         // Show a value from the array on screen
         showText("Hello, " + peopleInClass[2], 400, 170);
+        
+        // How many frames have been animated?
+        showText("Frames: " + frames, 100, 50);
+        
+        // Track frame count
+        frames += 1;        
     }
 }
