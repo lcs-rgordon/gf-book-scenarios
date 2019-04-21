c2db7d7c1bd8c0314808c7c3b6a53ef8f0eccde0
Russell Gordon <rgordon@lcs.on.ca>
2019-04-21 12:42:46 -0400

    Fix problem so that piano key sound played once only

--- chapter06/piano-1/Key.java
+++ chapter06/piano-1/Key.java
@@ -3,10 +3,18 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Key extends Actor
 {
     /**
+     * Instance variables / objects
+     * (can be used by any method in this class)
+     */
+    private boolean isDown;
+    
+    /**
      * Create a new key.
      */
     public Key()
     {
+        // Key object begins life in the "up" position
+        isDown = false;
     }
 
     /**
@@ -14,14 +22,21 @@ public class Key extends Actor
      */
     public void act()
     {
-        if ( Greenfoot.isKeyDown("g") )
+        // When the piano key is not down, and the keyboard key was just pressed
+        if ( isDown == false && Greenfoot.isKeyDown("g") == true)
         {
+            // Make the piano key appear to be down and play the sound
             setImage("white-key-down.png");
             play();
+            isDown = true;
         }
-        else
+        
+        // When the piano key is down, and the keyboard key was just released
+        if ( isDown == true && Greenfoot.isKeyDown("g") == false )
         {
+            // Make the piano key appear to be up
             setImage("white-key.png");
+            isDown = false;
         }
     }
     
